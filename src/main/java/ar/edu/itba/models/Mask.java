642143a37e3ad6afa00bacf565473821bc2d07fd
root <ncastano@itba.edu.ar>
2017-08-28 21:36:35 -0300

    mascara

---
+++ src/main/java/ar/edu/itba/models/Mask.java
@@ -0,0 +1,126 @@
+package ar.edu.itba.models;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by root on 8/28/17.
+ */
+public class Mask {
+
+    double mask[][];
+    protected int size;
+    protected List<Integer> list;
+
+    public Mask(int size){
+        mask= new double[size][size];
+        this.size = size;
+        list = new ArrayList<>();
+    }
+
+    private double get(int x, int y)throws IllegalArgumentException{
+        if(x >=0 && x < size && y>=0 && y<size){
+            return mask[x][y];
+        }else{
+            throw new IllegalArgumentException("x or y out of bounds");
+        }
+    }
+
+    public void set(int x, int y, double value)throws IllegalArgumentException{
+        if(x >=0 && x < size && y>=0 && y<size){
+            mask[x][y] = value;
+        }else{
+            throw new IllegalArgumentException("x or y out of bounds");
+        }
+
+    }
+
+    public String toString(){
+        StringBuffer buffer=new StringBuffer();
+        buffer.append("Mask values:\n");
+        for(double[] currentLine:mask){
+            buffer.append(Arrays.toString(currentLine));
+            buffer.append("\n");
+        }
+        return buffer.toString();
+
+    }
+
+    public double filter(double[] values,double[][] m) {
+        double accum = 0.0;
+        for(int i =0; i<size; i++){
+            for (int j = 0; j < size; j++) {
+                accum += m[i][j] * values[i*size + j];
+            }
+        }
+        System.out.println("VOy a devolver: " + accum);
+        return accum;
+    }
+
+    public double[][] filterImage(double[][] image){
+        double[][] newImage = new double[image.length][image[0].length];
+        int halfSize = (size-1)/2;
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                if(i<halfSize || j<halfSize || i>(image.length-1-halfSize) || j>(image[i].length-1-halfSize)){
+                    newImage[i][j] = image[i][j];
+                } else {
+                    double[] values = new double[size*size];
+                    for (int k = 0; k < size; k++) {
+                        for (int k2 = 0; k2 < size; k2++) {
+                            values[k*size+k2] = image[i+k-halfSize][j+k2-halfSize];
+                            System.out.print("\t" + values[k*size+k2]);
+
+                        }
+                    }
+                    System.out.println("Esto me devuelve filter: " + filter(values,mask));
+                    newImage[i][j] = filter(values,mask);
+
+                }
+            }
+        }
+        return newImage;
+    }
+
+
+    public static void main(String[] args) {
+        double image[][] = new double[5][5];
+        Mask mask = new Mask(3);
+        int k=1;
+
+        for(int i=0; i<5;i++) {
+            for (int j = 0; j < 5; j++) {
+                image[i][j]=k++;
+            }
+        }
+
+        for(int i=0; i<5;i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(image[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
+        image= mask.filterImage(image);
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+//        for(int i=0; i<5;i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print(image[i][j]+"\t");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//        System.out.println(test.toString());
+
+    }
+
+}
+
